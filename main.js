a3a0fc1cfab532989b7aac775a7d7de4531e8c42
Daniel Zamora Paez <[email>
2025-07-16 14:51:33 -0500

    Operaciones matematicas

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-import {PI, IVA} from './constantes.js'
+import {PI, IVA, DIAS_SEMANA} from './constantes.js'
 
-function calcular(PI, radio){    
+//Ejercicio 1.1
+function areaCirculoMain(PI, radio){    
     return PI*(radio*radio);
 }
+console.log("Area del circulo: ",areaCirculoMain(PI,31));
 
-console.log(calcular(PI,31));
+//Ejercicio 1.2
+function precioIva(IVA, precio) {
+    return IVA*precio
+}
+console.log("Precio IVA de 100:",precioIva(IVA, 100));
+
+//Ejercicio 1.3
+console.log(DIAS_SEMANA[2]);
+
+/*EJERCICIO 2 */
+import {sumar, restar, multiplicar, areaCirculo} from './operaciones_matematicas.js'
---
+++ operaciones_matematicas.js
@@ -0,0 +1,15 @@
+function sumar(a,b){
+    return a+b
+}
+
+function restar(a,b){
+    return a-b
+}
+
+function multiplicar(a,b){
+    return a*b
+}
+
+function areaCirculo(PI, radio){    
+    return PI*(radio*radio);
+}
